3b8bd535eb5d979819eb1de3e18b3a88dc977692
Arli <111626295+vovmomyt@users.noreply.github.com>
2025-02-13 14:54:44 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/luyba",
+  base: "/RASHAD",
   plugins: [react()],
   build: {
     assetsDir: 'assets',
